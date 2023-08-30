24128961234cb946d58458bb84051e37787c01ef
VishnuWhitehatJr <133128285+VishnuWhitehatJr@users.noreply.github.com>
2023-08-30 17:59:51 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC1f9CfUxmfESf50Xlc5_q9rQRMY0OaCJM",
+    authDomain: "let-s-chat-4a982.firebaseapp.com",
+    databaseURL: "https://let-s-chat-4a982-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-4a982",
+    storageBucket: "let-s-chat-4a982.appspot.com",
+    messagingSenderId: "13372209456",
+    appId: "1:13372209456:web:fb9f16f059e2ae11ee30f5",
+    measurementId: "G-XJWZTH7Z2T"
+  };
+
+firebase.initializeApp(firebaseConfig);
